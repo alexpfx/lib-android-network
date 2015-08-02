fd1394a7ebf03da9ddca7fb1f51abffbc2893de1
alexpfx <alexpfba@gmail.com>
2015-08-02 15:15:18 -0300

    classe utils para views.

---
+++ lib/src/main/java/br/com/alexpfx/android/lib/network/view/common/Views.java
@@ -0,0 +1,15 @@
+package br.com.alexpfx.android.lib.network.view.common;
+
+import android.view.View;
+
+/**
+ * Created by alexandre on 02/08/15.
+ */
+public class Views {
+
+    public static <T> T getView(View parent, int resId, Class<T> type) {
+        return (T)
+                parent.findViewById(resId);
+    }
+
+}
