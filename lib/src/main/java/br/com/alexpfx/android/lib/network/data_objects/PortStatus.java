abc161e2c23e98f4ec75f60012160e37a2fe18b4
alexpfx <alexpfba@gmail.com>
2015-08-15 22:22:43 -0300

    refactorings: movendo pacotes, adicionando libs.

--- lib/src/main/java/br/com/alexpfx/android/lib/network/data_objects/PortStatus.java
+++
@@ -1,8 +0,0 @@
-package br.com.alexpfx.android.lib.network.data_objects;
-
-/**
- * Created by alexandre on 28/07/15.
- */
-public enum PortStatus {
-    OPEN, CLOSED
-}
--- lib/src/main/java/br/com/alexpfx/android/lib/network/model/Interactor.java
+++
@@ -1,9 +0,0 @@
-package br.com.alexpfx.android.lib.network.model;
-
-/**
- * Created by alexandre on 28/07/15.
- */
-//TODO: mover para projeto base.
-public interface Interactor {
-    void run();
-}
